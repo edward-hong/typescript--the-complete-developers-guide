4e6bf7a094c9d6b844b1cc12f4a9db46663a063b
edward-hong <edward.hong527@gmail.com>
2021-05-26 00:38:44 +1200

    S11L33 Putting It All Together

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,9 +1,13 @@
 import { MatchReader } from './MatchReader'
 import { CsvFileReader } from './CsvFileReader'
-import { MatchResult } from './MatchResult'
+import { ConsoleReport } from './reportTargets/ConsoleReport'
+import { WinsAnalyzer } from './analyzers/WinsAnalysis'
+import { Summary } from './Summary'
 
 const csvFileReader = new CsvFileReader('football.csv')
 const matchReader = new MatchReader(csvFileReader)
 matchReader.load()
 
-console.log(`Man United won ${manUnitedWins} games`)
+const summary = new Summary(new WinsAnalyzer('Man United'), new ConsoleReport())
+
+summary.buildAndPrintReport(matchReader.matches)

e636e99ca0750b8d359a1d5846dcad9f5dadad64
edward-hong <edward.hong527@gmail.com>
2021-05-25 22:26:02 +1200

    S11L14 Type Assertions

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,5 +1,6 @@
 import fs from 'fs'
 
+import { MatchResult } from './MatchResult'
 import { dateStringToDate } from './utls'
 
 export class CsvFileReader {
@@ -23,6 +24,8 @@ export class CsvFileReader {
           row[2],
           parseInt(row[3]),
           parseInt(row[4]),
+          row[5] as MatchResult,
+          row[6],
         ]
       })
   }

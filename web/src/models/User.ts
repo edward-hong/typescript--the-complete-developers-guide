35f35a9a4b4a5ad1aef848707f1580a26f05561e
edward-hong <edward.hong527@gmail.com>
2021-05-26 17:45:54 +1200

    S13L10 Dynamic Array Creation

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -3,7 +3,7 @@ interface UserProps {
   age?: number
 }
 
-type Callback = () => {}
+type Callback = () => void
 
 export class User {
   constructor(private data: UserProps) {}
@@ -18,5 +18,9 @@ export class User {
     Object.assign(this.data, update)
   }
 
-  on(eventName: string, callback: Callback) {}
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || []
+    handlers.push(callback)
+    this.events[eventName] = handlers
+  }
 }

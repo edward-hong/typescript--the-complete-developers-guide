e13078b28cf047d816e640677528d7f1702d3331
edward-hong <edward.hong527@gmail.com>
2021-05-26 21:27:01 +1200

    S13L16 Saving User Data

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,8 +1,5 @@
 import { User } from './models/User'
 
-const user = new User({ id: 1 })
-user.fetch()
+const user = new User({ name: 'new record', age: 0 })
 
-setTimeout(() => {
-  console.log(user)
-}, 4000)
+user.save()

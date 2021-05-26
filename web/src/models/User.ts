72c8220bf4176c41240f55726c599aeed4cf0fcb
edward-hong <edward.hong527@gmail.com>
2021-05-26 23:19:56 +1200

    S13L30 Re-Integrating Attributes

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,5 +1,6 @@
 import { Eventing } from './Eventing'
 import { Sync } from './Sync'
+import { Attributes } from './Attributes'
 
 export interface UserProps {
   id?: number
@@ -10,6 +11,11 @@ export interface UserProps {
 const rootUrl = 'http://localhost:3000/users'
 
 export class User {
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs)
+  }
+
   public events: Eventing = new Eventing()
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl)
+  public attributes: Attributes<UserProps>
 }

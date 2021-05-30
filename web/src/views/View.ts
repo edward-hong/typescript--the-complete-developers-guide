ff7ab1ef6b91743ca5f5e646758b4d41cd7dcf6e
edward-hong <edward.hong527@gmail.com>
2021-05-30 17:47:16 +1200

    S13L67 Mapping Regions

--- web/src/views/View.ts
+++ web/src/views/View.ts
@@ -5,7 +5,13 @@ export abstract class View<T extends Model<K>, K> {
     this.bindModel()
   }
 
+  regions: { [key: string]: Element } = {}
+
   abstract template(): string
+
+  regionsMap(): { [key: string]: string } {
+    return {}
+  }
 
   eventsMap(): { [key: string]: () => void } {
     return {}
@@ -29,6 +35,19 @@ export abstract class View<T extends Model<K>, K> {
     }
   }
 
+  mapRegions(fragment: DocumentFragment): void {
+    const regionsMap = this.regionsMap()
+
+    for (let key in regionsMap) {
+      const selector = regionsMap[key]
+      const element = fragment.querySelector(selector)
+
+      if (element) {
+        this.regions[key] = element
+      }
+    }
+  }
+
   render(): void {
     this.parent.innerHTML = ''
 
@@ -36,6 +55,7 @@ export abstract class View<T extends Model<K>, K> {
     templateElement.innerHTML = this.template()
 
     this.bindEvents(templateElement.content)
+    this.mapRegions(templateElement.content)
 
     this.parent.append(templateElement.content)
   }

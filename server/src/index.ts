7a2b07c50ef451db7a7a7bed1ebea8073d49d329
edward-hong <edward.hong527@gmail.com>
2021-05-31 20:42:38 +1200

    S14L10 Wiring Up Sessions

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from 'express'
+import cookieSession from 'cookie-session'
 
 import { router } from './routes/loginRoutes'
 
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
+app.use(cookieSession({ keys: ['aslkdfjas'] }))
 app.use(router)
 
 app.listen(3000, () => {

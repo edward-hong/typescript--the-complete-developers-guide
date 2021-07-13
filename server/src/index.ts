06958f5f862070bb70b3e0ad507347666705e077
Edward Hong <edward.hong527@gmail.com>
2021-07-13 16:15:29 +1200

    S16L20 Fixing Routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,14 @@
 import express, { Request, Response } from 'express'
 import cookieSession from 'cookie-session'
 
-import { router } from './routes/loginRoutes'
 import { AppRouter } from './AppRouter'
 import './controllers/LoginController'
+import './controllers/RootController'
 
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieSession({ keys: ['aslkdfjas'] }))
-app.use(router)
 app.use(AppRouter.getInstance())
 
 app.listen(3000, () => {

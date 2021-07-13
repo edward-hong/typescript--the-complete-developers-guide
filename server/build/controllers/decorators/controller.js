a71396283f475bd7507077fe9e6f6cb2d0e85126
Edward Hong <edward.hong527@gmail.com>
2021-07-13 15:54:35 +1200

    S16L17 Body Validators

--- server/build/controllers/decorators/controller.js
+++ server/build/controllers/decorators/controller.js
@@ -9,6 +9,9 @@ exports.controller = void 0;
 require("reflect-metadata");
 var AppRouter_1 = require("../../AppRouter");
 var MetadataKeys_1 = require("./MetadataKeys");
+function bodyValidators(keys) {
+    return function (req, res, next) { };
+}
 function controller(routePrefix) {
     return function (target) {
         var router = AppRouter_1.AppRouter.getInstance();

4a8f4232fd62d8f7244d1736dd072efaa3593f9a
Edward Hong <edward.hong527@gmail.com>
2021-07-13 15:58:02 +1200

    S16L18 Automated Validation

--- server/src/controllers/decorators/controller.ts
+++ server/src/controllers/decorators/controller.ts
@@ -6,7 +6,21 @@ import { Methods } from './Methods'
 import { MetadataKeys } from './MetadataKeys'
 
 function bodyValidators(keys: string): RequestHandler {
-	return function (req: Request, res: Response, next: NextFunction) {}
+	return function (req: Request, res: Response, next: NextFunction) {
+		if (!req.body) {
+			res.status(422).send('Invalid request')
+			return
+		}
+
+		for (let key of keys) {
+			if (!req.body[key]) {
+				res.status(422).send('Invalid request')
+				return
+			}
+		}
+
+		next()
+	}
 }
 
 export function controller(routePrefix: string) {
@@ -26,8 +40,18 @@ export function controller(routePrefix: string) {
 				Reflect.getMetadata(MetadataKeys.middleware, target.prototype, key) ||
 				[]
 
+			const requiredBodyProps =
+				Reflect.getMetadata(MetadataKeys.validator, target.prototype, key) || []
+
+			const validator = bodyValidators(requiredBodyProps)
+
 			if (path) {
-				router[method](`${routePrefix}${path}`, ...middlewares, routeHandler)
+				router[method](
+					`${routePrefix}${path}`,
+					...middlewares,
+					validator,
+					routeHandler
+				)
 			}
 		}
 	}
